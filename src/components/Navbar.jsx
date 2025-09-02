058e7ce7c1386b41983eb3ed1cb13d97766db07d
Anna Yan <annayan@Annas-MacBook-Air.local>
2025-09-02 15:33:59 -0400

    logo at top left

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import IconImage from '../assets/icon.png'
 
 const Navbar = () => {
     return (
         <nav className='bg-black text-white px-8 md:px-16 lg:px-24'>
             <div className='container py-2 flex justify-center md:justify-between items-center'>
-                <div className='text-2xl font-bold hidden md:inline'>Anna</div>
+                {/* <div className='text-2xl font-bold hidden md:inline'>Anna</div> */}
+                <img src={IconImage} alt='logo' className='w-15 h-13 rounded-full object-cover hidden md:inline'/>
                 <div className='space-x-6'>
                     <a href="#home" className='hover:text-gray-400'>Home</a>
                     <a href="#about" className='hover:text-gray-400'>About Me</a>
