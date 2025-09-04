048ab8836a97c5fff046deb30b358108d5a51e52
Anna Yan <annayan@Annas-MacBook-Air.local>
2025-09-03 22:21:04 -0400

    reading list added and consistent hover size

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,27 +9,21 @@ const About = () => {
                 <h2 className='text-4xl font-bold text-center mb-12'>About Me</h2>
                 <div className='flex flex-col md:flex-row items-center md:space-x-12'>
                     <img src={AboutImage} alt='About Me Image'
-                    className='w-72 h-80 rounded object-cover mb-8 md:mb-0'/>
+                    className='w-100 h-120 rounded object-cover mb-8 md:mb-0'/>
                     <div className='flex-1'>
                         <p className='text-lg mb-8'>
                             I am a passionate computer science student at the University of Chicago, 
                             eager to explore the world of technology and innovation. My journey 
                             into coding began in high school, where I discovered my love for problem-solving 
                             and creating digital solutions. Since then, I have honed my skills in 
-                            various programming languages and frameworks, including JavaScript, Python, and React.
+                            various programming languages and frameworks, including Python, Java, and React. Outside
+                            of academics, I am a competitive hip hop dancer, an avid coffee and matcha drinker, and 
+                            I enjoy hiking and traveling!
                         </p>
                         {/* Skills progress bars */}
                         <div className='space-y-4'>
                             <div className='flex items-center'>
-                                <label htmlFor="htmlandcss" className='w-2/12'>HTML & CSS</label>
-                                <div className="grow bg-gray-800 rounded-full h-2.5">
-                                    <div className='bg-gradient-to-r from-rose-400 to-blue-500 h-2.5 
-                                        rounded-full transform transition-transform duration-300 hover:scale-105 w-10/12'>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className='flex items-center'>
-                                <label htmlFor="reactjs" className='w-2/12'>React JS</label>
+                                <label htmlFor="htmlandcss" className='w-2/12'>Python</label>
                                 <div className="grow bg-gray-800 rounded-full h-2.5">
                                     <div className='bg-gradient-to-r from-rose-400 to-blue-500 h-2.5 
                                         rounded-full transform transition-transform duration-300 hover:scale-105 w-11/12'>
@@ -37,7 +31,15 @@ const About = () => {
                                 </div>
                             </div>
                             <div className='flex items-center'>
-                                <label htmlFor="nodejs" className='w-2/12'>Node JS</label>
+                                <label htmlFor="reactjs" className='w-2/12'>Java</label>
+                                <div className="grow bg-gray-800 rounded-full h-2.5">
+                                    <div className='bg-gradient-to-r from-rose-400 to-blue-500 h-2.5 
+                                        rounded-full transform transition-transform duration-300 hover:scale-105 w-10/12'>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='flex items-center'>
+                                <label htmlFor="nodejs" className='w-2/12'>React JS</label>
                                 <div className="grow bg-gray-800 rounded-full h-2.5">
                                     <div className='bg-gradient-to-r from-rose-400 to-blue-500 h-2.5 
                                         rounded-full transform transition-transform duration-300 hover:scale-105 w-9/12'>
@@ -64,6 +66,24 @@ const About = () => {
                                 </h3>
                                 <p>Tools Learned</p>
                             </div>
+                        </div>
+                        {/* Reading list */}
+                        <div className='mt-12'>
+                            <h3 className='text-xl font-bold mb-4'>Currently Reading...</h3>
+                            <div className='justify-between text-center'>
+                                <ul className='list-disc list-inside space-y-2 text-white'>
+                                <li>
+                                        <span className='italic'>The Idiot</span> by Elif Bautman
+                                </li>
+                                <li>
+                                        <span className='italic'>Jane Eyre</span> by Charlotte Brontë
+                                </li>
+                                <li>
+                                        <span className='italic'>Intermezzo</span> by Sally Rooney
+                                </li>
+                                </ul>
+                            </div>
+                            
                         </div>
                     </div>
                 </div>
