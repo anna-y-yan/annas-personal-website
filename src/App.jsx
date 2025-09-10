287789da8ae05d435c9f38227cfdb604dbde23ee
Anna Yan <annayan@Annas-MacBook-Air.local>
2025-09-10 14:17:14 -0400

    vercel analytics import

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ function App() {
       <Projects />
       <Contact />
       <Footer />
+      <Analytics />
       {/* <h1 className="font-pollerone text-[50px] text-rose-600 text-center">Hello World!</h1>
       <h1 className="font-DMSerif text-[50px] text-violet-400 hover:text-sky-600 text-center">My name is Anna and this is my website</h1> */}
     </div>
